19c1a98e009ffc68a09ae61c66b16a3eed28872b
Juan Manuel Alvarez Bermudez <105917953+Sirio2022@users.noreply.github.com>
2024-08-18 12:33:17 -0500

    Starting point.
    Creada la base para comenzar a trabajar.  No olvidar Hacer commits a la rama Dev.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className=" grid min-h-screen content-center">
+      <h1 className=" text-4xl font-bold text-center pt-5">
+        Arkanoid - Official WebPage
+      </h1>
+    </div>
+  );
+}
+
+export default App;
